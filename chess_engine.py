8e9319bd7d0f2749123a2befa21b0e9efc5d97fb
Vince Munya <vincentm43@gmail.com>
2023-03-09 11:47:32 +0200

    Update chess_engine.py

--- chess_engine.py
+++ chess_engine.py
@@ -2,7 +2,6 @@
 # The Chess Board class
 # Will store the state of the chess game, print the chess board, find valid moves, store move logs.
 #
-# Note: move log class inspired by Eddie Sharick
 #
 from Piece import Rook, Knight, Bishop, Queen, King, Pawn
 from enums import Player
